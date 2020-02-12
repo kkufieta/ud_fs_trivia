7477575eef6c8fbd7381daecc2ddf577741a42f9
kkufieta <katharina.kufieta@gmail.com>
2020-02-12 08:07:35 -0500

    Update comments for questions endpoing

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -63,7 +63,7 @@ def create_app(test_config=None):
           'error': 404,
           'message': 'resource not found'
       }), 404
-        
+
   @app.errorhandler(422)
   def unprocessable_entity(error):
         return jsonify({
@@ -72,17 +72,11 @@ def create_app(test_config=None):
           'message': 'unprocessable entity'
         }), 422
         
+        
   '''
-  @TODO: 
-  Create an endpoint to handle GET requests for questions, 
-  including pagination (every 10 questions). 
-  This endpoint should return a list of questions, 
-  number of total questions, current category, categories. 
-
-  TEST: At this point, when you start the application
-  you should see questions and categories generated,
-  ten questions per page and pagination at the bottom of the screen for three pages.
-  Clicking on the page numbers should update the questions. 
+  Endpoint that handles GET requests for questions, including pagination.
+  Returns a list of questions, number of total questions, current category,
+  and a dict of all categories.
   '''
   @app.route('/questions')
   def get_questions():
@@ -97,8 +91,6 @@ def create_app(test_config=None):
         categories_dict = {}
         for category in categories:
               categories_dict[category.id] = category.type
-
-        # question: id, question, answer, category, difficulty
 
         return jsonify({
           'success': True,
